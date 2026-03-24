0815ff57e6a170856597281bfca0b3b6d238f0c7
aravind-sketch <you@example.com>
2026-03-24 01:00:01 -0700

    i added some if condition

--- sample.py
+++ sample.py
@@ -1,13 +1,15 @@
-# Simple program to read a file and print its contents
-
 def read_file(file_name):
     try:
         with open(file_name, 'r') as file:
             data = file.read()
-            print("File Content:")
-            print(data)
+            
+            if "data" in data:
+                print("Required word found!")
+                print(data)
+            else:
+                print("Word 'data' not found in file.")
+
     except FileNotFoundError:
         print("File not found!")
 
-# Call the function
 read_file("sample.txt")
